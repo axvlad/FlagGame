cf5133753bf4db9f347ade34121e5a7a60cb5c11
Vlad Burma <burmavburma@gmail.com>
2023-02-25 11:36:25 +0200

    Initial Commit

---
+++ TrueFlags/TrueFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueFlagsApp.swift
+//  TrueFlags
+//
+//  Created by Vlad on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrueFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
